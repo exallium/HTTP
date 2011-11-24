eb0573170588be33a1f6353e208357e7eaa9423a
Alex Hart <alex.hart658@gmail.com>
2011-11-23 20:24:20 -0400

    Add ability to read to end of a resource.

--- http_imp.c
+++ http_imp.c
@@ -10,13 +10,41 @@
 
 #define DEBUG
 
-// http_head
+// pulls information from the server, fills in necessary info
+static int process_response(struct HTTP_CONNECTION *conn, 
+        struct HTTP_RESPONSE* response) {
+    char buffer[1024];
+    int count, i, n;
+    fd_set readfds;
+    struct timeval tv;
+
+    FD_ZERO(&readfds);
+    FD_SET(conn->sd, &readfds);
+
+    n = conn->sd + 1;
+    tv.tv_sec = 0;
+    tv.tv_usec = 500000;
+    
+    while(select(n, &readfds, NULL, NULL, &tv) != -1 && FD_ISSET(conn->sd, &readfds)) {
+        count = read(conn->sd, buffer, 1024);
+        for(i = 0; i < count; i++) {
+#ifdef DEBUG
+            putchar(buffer[i]);
+#endif
+        } 
+    }
+
+    return 0;
+}
+
+// http_get
 // sends <type> request to a server
 // Simple enough.
 int http_request(struct HTTP_CONNECTION* conn, 
         struct HTTP_RESPONSE* response,
         char* type, char* resource, char* data) {
     char *buffer;
+
     int len = 32 + strlen(conn->hostname) + strlen(resource);
     buffer = (char *)malloc(sizeof(char) * len);
 
@@ -35,5 +63,10 @@ int http_request(struct HTTP_CONNECTION* conn,
     printf("d:http_request:generated\n%s", buffer);
 #endif
 
+    send(conn->sd, buffer, strlen(buffer), 0);
+
+    process_response(conn, response);
+
     return 0;
 }
+
